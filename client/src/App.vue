4bc4e955339fd0d4f8b8956c6aae472466c1d3d7
ebadia <enric.badia@gmail.com>
2018-03-26 16:38:42 +0200

    added header anf footer botstrap 4 components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <router-view/>
+      <Header/>
+      <!-- <img src="./assets/logo.png"> -->
+      <router-view/>
+      <Footer/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
